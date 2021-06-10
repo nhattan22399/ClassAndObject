5a55236f1296fc800a5127a890f015b9735a900b
nhattan22399 <linhatan99@gmail.com>
2021-06-10 10:39:53 +0700

    first commit

---
+++ src/advance/dev/MainApp.java
@@ -0,0 +1,37 @@
+package advance.dev;
+
+import java.util.*;
+
+import java.util.Scanner;
+
+
+public class MainApp {
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Nhap thong tin sinh vien");
+		Student std = new Student();
+		System.out.println("NHap vao ho ten: ");
+		std.name = sc.next();
+		System.out.println("NHap vao tuoi: ");
+		std.age = sc.nextInt();
+		System.out.println("NHap vao dia chi: ");
+		std.adress = sc.next();
+		System.out.println("NHap vao sdt: ");
+		std.phone = sc.next();
+		System.out.println("NHap vao dtb: ");
+		std.dtb = sc.nextDouble();
+		
+		System.out.println(std.name+" "+std.age+" "+std.adress+" "+std.phone+" "+std.dtb);
+		
+		
+		
+		
+		
+		
+	}
+
+}
---
+++ src/advance/dev/Student.java
@@ -0,0 +1,9 @@
+package advance.dev;
+
+public class Student {
+	public String name;
+	public int age;
+	public String adress;
+	public String phone;
+	public double dtb;
+}
